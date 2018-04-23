101c0ed4b652af9ef76d55fa00f6a9ac00e71e5b
kamilo116 <kamil.sladowski@gmail.com>
2018-04-23 23:58:39 +0200

    added grpc client

--- src/sample/DisplayImagePoll.java
+++ src/sample/DisplayImagePoll.java
@@ -1,5 +1,7 @@
 package sample;
 
+import io.grpc.examples.imageviewer.ImageDataSample;
+import io.grpc.examples.imageviewer.ImagePollClient;
 import javafx.animation.*;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -24,44 +26,46 @@ import java.util.concurrent.atomic.AtomicReference;
 
 
 public class DisplayImagePoll extends Application {
-    private final String IMAGES_PATH = "D:\\Studia\\GAPED_2\\GAPED\\GAPED\\";
-
     private final Map<String, Integer> IMAGE_CATEGORY = Map.of(
             "A",100,"H",100,"N",100,"P",
             100,"Sn",100, "Sp",100);
     private final Integer img_num = 600;
-
-    private Duration displayDur1;
-    private Duration displayDur2;
-    private Duration displayDur3;
-    private Duration blackDur1;
-    private Duration blackDur2;
-    private Duration blackDur3;
+    private String IMAGES_PATH;
+    private Duration[] displayDur = new Duration[3];
+    private Duration[] blackDur = new Duration[3];
     private List<String> images;
     private Iterator<String> imageIterator;
     private AtomicReference<ImagePattern> img_pattern;
-    private Scene sc;
+    private String BLACK_SCREEN_NAME = "Black";
 
-    public void loadDisplayDurations(){
+    public void loadConfigurationData(){
         Properties props = new Properties();
         try {
             props.load(new FileInputStream(new File("resources\\input.cfg")));
+            IMAGES_PATH = props.getProperty("IMAGES_PATH", "");
             int first_img_display_dur = Integer.valueOf(props.getProperty("FIRST_IMG_DISPLAY_DUR", "1"));
-            int first_black_background = first_img_display_dur + Integer.valueOf(props.getProperty("FIRST_BLACK_BACKGROUND", "1"));
-            int second_img_display_dur = first_black_background + Integer.valueOf(props.getProperty("SECOND_IMG_DISPLAY_DUR", "1"));
-            int second_black_background = second_img_display_dur + Integer.valueOf(props.getProperty("SECOND_BLACK_BACKGROUND", "1"));
-            int third_img_display_dur  = second_black_background + Integer.valueOf(props.getProperty("THIRD_IMG_DISPLAY_DUR", "1"));
-            int third_black_background = third_img_display_dur + Integer.valueOf(props.getProperty("THIRD_BLACK_BACKGROUND", "1"));
-            displayDur1 = Duration.seconds(first_img_display_dur);
-            displayDur2 = Duration.seconds(second_img_display_dur);
-            displayDur3 = Duration.seconds(third_img_display_dur);
-            blackDur1 = Duration.seconds(first_black_background);
-            blackDur2 = Duration.seconds(second_black_background);
-            blackDur3 = Duration.seconds(third_black_background);
+            int first_black_background = first_img_display_dur
+                    + Integer.valueOf(props.getProperty("FIRST_BLACK_BACKGROUND", "1"));
+            int second_img_display_dur = first_black_background
+                    + Integer.valueOf(props.getProperty("SECOND_IMG_DISPLAY_DUR", "1"));
+            int second_black_background = second_img_display_dur
+                    + Integer.valueOf(props.getProperty("SECOND_BLACK_BACKGROUND", "1"));
+            int third_img_display_dur  = second_black_background
+                    + Integer.valueOf(props.getProperty("THIRD_IMG_DISPLAY_DUR", "1"));
+            int third_black_background = third_img_display_dur
+                    + Integer.valueOf(props.getProperty("THIRD_BLACK_BACKGROUND", "1"));
+            displayDur[0] = Duration.seconds(first_img_display_dur);
+            displayDur[1] = Duration.seconds(second_img_display_dur);
+            displayDur[2] = Duration.seconds(third_img_display_dur);
+            blackDur[0] = Duration.seconds(first_black_background);
+            blackDur[1] = Duration.seconds(second_black_background);
+            blackDur[2] = Duration.seconds(third_black_background);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
     private String[] getImagesPaths(){
         String[] images_locks = new String[img_num];
         int i =0;
@@ -89,62 +93,106 @@ public class DisplayImagePoll extends Application {
         try {
             Path destFile = Paths.get("resources\\log.txt");
 
-            Files.write(destFile, Collections.singleton(img_name), Charset.forName("UTF-8"), StandardOpenOption.APPEND);
-            Files.write(destFile, Collections.singleton(date), Charset.forName("UTF-8"), StandardOpenOption.APPEND);
+            Files.write(destFile, Collections.singleton(img_name), Charset.forName("UTF-8"),
+                    StandardOpenOption.APPEND);
+            Files.write(destFile, Collections.singleton(date), Charset.forName("UTF-8"),
+                    StandardOpenOption.APPEND);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void switchImage(Iterator<String> imageIterator){
-            try {
-                String img_name = imageIterator.next();
-                System.out.printf(img_name);
-                //img_name = "D:\\Studia\\GAPED_2\\GAPED\\GAPED\\N\\N005.bmp";
-                img_pattern.set(new ImagePattern(new Image(new File(img_name).toURI().toString())));
-                sc.setFill(img_pattern.get());
-                writeToFile(img_name, getDate());
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
-            } catch (java.lang.NullPointerException e) {
-                e.printStackTrace();
-            }
+
+    private void switchImage(Iterator<String> imageIterator, ImagePollClient client, Scene scene){
+        try {
+            String img_name = imageIterator.next();
+            System.out.println(img_name);
+            img_pattern.set(new ImagePattern(new Image(new File(img_name).toURI().toString())));
+            scene.setFill(img_pattern.get());
+            writeToFile(img_name, getDate());
+            sendImageDataViaProtobuffers( client, img_name, getDate());
+        } catch (IllegalArgumentException | NullPointerException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void start(Stage stage) {
-        String[] ff = getImagesPaths();
-        images = new ArrayList<>(Arrays.asList(ff));
-        img_pattern = new AtomicReference<>(new ImagePattern(new Image(new File(images.get(0)).toURI().toString())));
-        BorderPane pane = new BorderPane();
-        sc = new Scene(pane);
+    private ImagePollClient runGRPCClient(){
+        return new ImagePollClient("localhost", 50051);
+    }
 
-        loadDisplayDurations();
-        sc.setFill(Color.BLACK);
-        imageIterator = images.iterator();
-        Collections.shuffle(images);
 
+    private void sendImageDataViaProtobuffers(ImagePollClient client, String img_name, String date){
+        boolean negative = false, neutral = false, positive = false, black = false;
+        int prefixLen = IMAGES_PATH.length();
+
+        if(BLACK_SCREEN_NAME.equals(img_name)) {
+            black = true;
+        }
+        else {
+            String img_short_name = img_name.substring(prefixLen, img_name.length());
+            if (img_short_name.contains("N"))
+                neutral = true;
+            else if (img_short_name.contains("P"))
+                positive = true;
+            else if (img_short_name.contains("Sn") ||
+                    img_short_name.contains("Sp") ||
+                    img_short_name.contains("H") ||
+                    img_short_name.contains("A"))
+                negative = true;
+        }
+        client.packImageData(new ImageDataSample(img_name, negative, neutral, positive, black));
+    }
+
+
+    private void runImageAnimation(ImagePollClient grpcClient, Scene scene){
         Timeline timelineShow = new Timeline(
-                new KeyFrame(displayDur1, e -> switchImage(imageIterator)),
-                new KeyFrame(blackDur1, e -> sc.setFill(Color.BLACK)),
-                new KeyFrame(displayDur2, e -> switchImage(imageIterator)),
-                new KeyFrame(blackDur2, e -> sc.setFill(Color.BLACK)),
-                new KeyFrame(displayDur3, e -> switchImage(imageIterator)),
-                new KeyFrame(blackDur3, e -> sc.setFill(Color.BLACK))
+                new KeyFrame(displayDur[0], e -> switchImage(imageIterator, grpcClient, scene)),
+                new KeyFrame(blackDur[0], e -> {
+                    scene.setFill(Color.BLACK);
+                    sendImageDataViaProtobuffers(grpcClient, BLACK_SCREEN_NAME, getDate());
+                }),
+                new KeyFrame(displayDur[1], e -> switchImage(imageIterator, grpcClient, scene)),
+                new KeyFrame(blackDur[1], e -> {
+                    scene.setFill(Color.BLACK);
+                    sendImageDataViaProtobuffers(grpcClient, BLACK_SCREEN_NAME, getDate());
+                }),
+                new KeyFrame(displayDur[2], e -> switchImage(imageIterator, grpcClient, scene)),
+                new KeyFrame(blackDur[2], e -> {
+                    scene.setFill(Color.BLACK);
+                    sendImageDataViaProtobuffers(grpcClient, BLACK_SCREEN_NAME, getDate());
+                })
         );
-
         timelineShow.setCycleCount(1);
         timelineShow.setOnFinished(event -> {
             Collections.shuffle(images);
             imageIterator= images.iterator();
             timelineShow.playFromStart();
         });
-
         timelineShow.play();
+    }
+
+
+    @Override
+    public void start(Stage stage) {
+        loadConfigurationData();
+        String[] imagesPaths = getImagesPaths();
+        images = new ArrayList<>(Arrays.asList(imagesPaths));
+        img_pattern = new AtomicReference<>(new ImagePattern(new Image(new File(images.get(0)).toURI().toString())));
+
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane);
+        scene.setFill(Color.BLACK);
+        imageIterator = images.iterator();
+        Collections.shuffle(images);
+        ImagePollClient grpcClient = runGRPCClient();
+
+        runImageAnimation(grpcClient, scene);
         stage.setFullScreen(true);
-        stage.setScene(sc);
+        stage.setScene(scene);
         stage.show();
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
